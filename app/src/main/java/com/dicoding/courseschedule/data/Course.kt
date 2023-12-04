9c9e6cbd65134c7817facf1d16510a855bd34a8b
saliharya <saliharyazinc@gmail.com>
2023-12-04 20:16:17 +0700

    complete todo 1

--- app/src/main/java/com/dicoding/courseschedule/data/Course.kt
+++ app/src/main/java/com/dicoding/courseschedule/data/Course.kt
@@ -1,12 +1,17 @@
 package com.dicoding.courseschedule.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 //TODO 1 : Define a local database table using the schema in app/schema/course.json
+@Entity(tableName = "course")
 data class Course(
-    val id: Int = 0,
-    val courseName: String,
-    val day: Int,
-    val startTime: String,
-    val endTime: String,
-    val lecturer: String,
-    val note: String
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @ColumnInfo(name = "title") val courseName: String,
+    @ColumnInfo(name = "title") val day: Int,
+    @ColumnInfo(name = "title") val startTime: String,
+    @ColumnInfo(name = "title") val endTime: String,
+    @ColumnInfo(name = "title") val lecturer: String,
+    @ColumnInfo(name = "title") val note: String
 )
