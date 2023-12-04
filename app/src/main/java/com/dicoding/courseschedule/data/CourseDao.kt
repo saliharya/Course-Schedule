0894a91fd3b448fe8bdae84c9cd127001bc05dcd
saliharya <saliharyazinc@gmail.com>
2023-12-04 20:21:31 +0700

    complete todo 2 and 3

--- app/src/main/java/com/dicoding/courseschedule/data/CourseDao.kt
+++ app/src/main/java/com/dicoding/courseschedule/data/CourseDao.kt
@@ -2,9 +2,11 @@ package com.dicoding.courseschedule.data
 
 import androidx.lifecycle.LiveData
 import androidx.paging.DataSource
+import androidx.room.Dao
 import androidx.sqlite.db.SupportSQLiteQuery
 
 //TODO 2 : Define data access object (DAO)
+@Dao
 interface CourseDao {
 
     fun getNearestSchedule(query: SupportSQLiteQuery): LiveData<Course?>
--- app/src/main/java/com/dicoding/courseschedule/data/CourseDatabase.kt
+++ app/src/main/java/com/dicoding/courseschedule/data/CourseDatabase.kt
@@ -1,10 +1,12 @@
 package com.dicoding.courseschedule.data
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
 //TODO 3 : Define room database class
+@Database(entities = [Course::class], version = 1)
 abstract class CourseDatabase : RoomDatabase() {
 
     abstract fun courseDao(): CourseDao
@@ -15,9 +17,9 @@ abstract class CourseDatabase : RoomDatabase() {
         private var instance: CourseDatabase? = null
 
         fun getInstance(context: Context): CourseDatabase {
-            return synchronized(this){
+            return synchronized(this) {
                 instance ?: Room.databaseBuilder(context, CourseDatabase::class.java, "courses.db")
-                        .build()
+                    .build()
             }
         }
 
